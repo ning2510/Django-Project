71e19642a87ec9ff6bd24edc76b56a2e983b87b3
ning2510 <1367560112@qq.com>
2020-11-03 15:48:57 +0800

    delete SECRET_KEY

--- Homework/settings.py
+++ Homework/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+=&=zc(1=p#klu4l+juje&ofv=(e(95c85qkuk4u)97-^ba9(7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
